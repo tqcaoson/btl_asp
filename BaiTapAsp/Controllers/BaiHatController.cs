a278e4cdbbeaa0bbd5c8c227e38021aa7172612d
anhanh2412 <anhanh2412>
2021-06-14 12:01:47 +0700

    add models

---
+++ BaiTapAsp/Controllers/BaiHatController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BaiTapAsp.Models;
+
+namespace BaiTapAsp.Controllers
+{
+    public class BaiHatController : Controller
+    {
+        // GET: BaiHat
+        public ActionResult showBH()
+        {
+            return View();
+        }
+
+        public ActionResult editBH (int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult editBH (BaiHat bh) {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult addBH (BaiHat bh) {
+            return View();
+        }
+
+        public ActionResult deleteBH(int id)
+        {
+            return View();
+        }
+    }
+}
