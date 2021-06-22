6d7ea0700633714270ab2208dab2b8395e0b60b8
Đỗ Ái Quốc <79491693+DoAiQuoc@users.noreply.github.com>
2021-06-22 15:45:28 +0700

    add Model

---
+++ BaiTapAsp/Models/NhacSi.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MusicManagement.Models
+{
+    public class NhacSi
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage ="Vui lòng nhập họ và tên nhạc sĩ")]
+        [Display(Name = "Tên nhạc sĩ")]
+        public string fullName { get; set; }
+        [Required(ErrorMessage = "Vui lòng chọn ảnh nhạc sĩ")]
+        [Display(Name = "Hình ảnh")]
+        public string image { get; set; }
+        [Required(ErrorMessage = "Vui lòng chọn ngày tháng năm sinh")]
+        [Display(Name = "Ngày sinh")]
+        public string dateofbirth { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập quê quán")]
+        [Display(Name = "Quê quán")]
+        public string address { get; set; }
+
+        // thuoc tinh de upload hinh len nhung k luu
+        [NotMapped]
+        public HttpPostedFileBase Imageupload { get; set; }
+        public NhacSi()
+        {
+            image = "~/Content/images/add.jpg";
+        }
+
+        public NhacSi(int id, string fullName, string image, string dateofbirth, string address)
+        {
+            this.id = id;
+            this.fullName = fullName;
+            this.image = image;
+            this.dateofbirth = dateofbirth;
+            this.address = address;
+        }
+
+        public NhacSi(string fullName, string image, string dateofbirth, string address)
+        {
+            this.fullName = fullName;
+            this.image = image;
+            this.dateofbirth = dateofbirth;
+            this.address = address;
+        }
+    }
+}
