fae2dbe6d252fefabd26bb1d90bb6f9491655645
Quang Huy <luanquanghuy@gmail.com>
2021-06-24 23:37:23 +0700

    add controller cshtml listAll DongNhac

--- BaiTapAsp/Models/DongNhac.cs
+++ BaiTapAsp/Models/DongNhac.cs
@@ -7,5 +7,13 @@ namespace BaiTapAsp.Models
 {
 	public class DongNhac
 	{
+		public int Id { get; set; }
+		public string TenDongNhac { get; set; }
+
+		public DongNhac(int id, string tenDongNhac)
+		{
+			Id = id;
+			TenDongNhac = tenDongNhac;
+		}
 	}
 }
