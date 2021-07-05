22ea3debcd93de58a93ee2df92d255221efd4e99
Quang Huy <luanquanghuy@gmail.com>
2021-07-05 11:16:11 +0700

    update validate

--- BaiTapAsp/Controllers/DongNhacController.cs
+++ BaiTapAsp/Controllers/DongNhacController.cs
@@ -7,47 +7,73 @@ using BaiTapAsp.Models;
 
 namespace BaiTapAsp.Controllers
 {
-    public class DongNhacController : Controller
-    {
-        DongNhacDAO dongnhacdao = new DongNhacDAO();
-        public ActionResult Index()
-        {
-			List<DongNhac> lists = dongnhacdao.getAllDongNhac();
-            return View(lists);
-        }
-
-        // GET: DongNhac/Add/
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Create(DongNhac dn)
-        {
-            dongnhacdao.insertDongNhac(dn);
-            return Redirect("/DongNhac");
-        }
-
-        // GET: DongNhac/Edit/
-        public ActionResult Edit(int id)
-        {
-            DongNhac dn = dongnhacdao.getDongNhacByID(id);
-            return View(dn);
-        }
-
-        // POST: DongNhac/Edit/
-        [HttpPost]
-        public ActionResult Edit(DongNhac dn)
-        {
-            dongnhacdao.updateDongNhac(dn);
-            return Redirect("/DongNhac");
-        }
-
-        public ActionResult Delete(int id)
+	public class DongNhacController : Controller
+	{
+		DongNhacDAO dongnhacdao = new DongNhacDAO();
+		public ActionResult Index()
 		{
-            dongnhacdao.deleteDongNhac(id);
-            return Redirect("/DongNhac");
+			if (Session["username"] == null)
+				return Redirect("/Auth/DangNhap");
+			List<DongNhac> lists = dongnhacdao.getAllDongNhac();
+			return View(lists);
 		}
-    }
+
+		// GET: DongNhac/Add/
+		public ActionResult Create()
+		{
+			if (Session["username"] == null)
+				return Redirect("/Auth/DangNhap");
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create(DongNhac dn)
+		{
+			if (Session["username"] == null)
+				return Redirect("/Auth/DangNhap");
+			if (ModelState.IsValid)
+			{
+				dongnhacdao.insertDongNhac(dn);
+				return Redirect("/DongNhac");
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+		// GET: DongNhac/Edit/
+		public ActionResult Edit(int id)
+		{
+			if (Session["username"] == null)
+				return Redirect("/Auth/DangNhap");
+			DongNhac dn = dongnhacdao.getDongNhacByID(id);
+			return View(dn);
+		}
+
+		// POST: DongNhac/Edit/
+		[HttpPost]
+		public ActionResult Edit(DongNhac dn)
+		{
+			if (Session["username"] == null)
+				return Redirect("/Auth/DangNhap");
+			if (ModelState.IsValid)
+			{
+				dongnhacdao.updateDongNhac(dn);
+				return Redirect("/DongNhac");
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+		public ActionResult Delete(int id)
+		{
+			if (Session["username"] == null)
+				return Redirect("/Auth/DangNhap");
+			dongnhacdao.deleteDongNhac(id);
+			return Redirect("/DongNhac");
+		}
+	}
 }
