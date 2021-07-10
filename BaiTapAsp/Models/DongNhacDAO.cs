70f20282f23eb69aebd54060c0297daae0fb4aa2
Quang Huy <luanquanghuy@gmail.com>
2021-07-10 18:09:19 +0700

    update error message

--- BaiTapAsp/Models/DongNhacDAO.cs
+++ BaiTapAsp/Models/DongNhacDAO.cs
@@ -15,83 +15,110 @@ namespace BaiTapAsp.Models
 			db = new DBConnection();
 		}
 
-        public List<DongNhac> getAllDongNhac()
-        {
-            List<DongNhac> listNhacSi = new List<DongNhac>();
-            try
-            {
-                string sql = "select * from Dong_nhac";           
-                DataTable dt = new DataTable();
-                SqlConnection con = db.getConnection();
-                SqlDataAdapter da = new SqlDataAdapter(sql, con);
-                con.Open();
-                da.Fill(dt);
-                con.Close();
-                DongNhac ns;
-                for (int i = 0; i < dt.Rows.Count; i++)
-                {
-                    int ID = Convert.ToInt32(dt.Rows[i]["Id"].ToString());
-                    String tenDongNhac = dt.Rows[i]["Ten_dongnhac"].ToString();
-                    ns = new DongNhac(ID, tenDongNhac);
-                    listNhacSi.Add(ns);
-                }
-            }catch (SqlException ex)
+		public List<DongNhac> getAllDongNhac()
+		{
+			List<DongNhac> listNhacSi = new List<DongNhac>();
+			try
 			{
-                Console.WriteLine("error getAllDongNhac: " + ex.Message);
-            }
-            return listNhacSi;
-        }
+				string sql = "select * from Dong_nhac";
+				DataTable dt = new DataTable();
+				SqlConnection con = db.getConnection();
+				SqlDataAdapter da = new SqlDataAdapter(sql, con);
+				con.Open();
+				da.Fill(dt);
+				con.Close();
+				DongNhac ns;
+				for (int i = 0; i < dt.Rows.Count; i++)
+				{
+					int ID = Convert.ToInt32(dt.Rows[i]["Id"].ToString());
+					String tenDongNhac = dt.Rows[i]["Ten_dongnhac"].ToString();
+					ns = new DongNhac(ID, tenDongNhac);
+					listNhacSi.Add(ns);
+				}
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine("error getAllDongNhac: " + ex.Message);
+			}
+			return listNhacSi;
+		}
 
-        public void insertDongNhac(DongNhac dn)
-        {
-            string sql = "insert into Dong_nhac (Ten_dongnhac) values (N'" + dn.TenDongNhac + "')";
-            SqlConnection con = db.getConnection();
-            SqlCommand cmd = new SqlCommand(sql, con);
-            con.Open();
-            cmd.ExecuteNonQuery();
-            cmd.Dispose();
-            con.Close();
-        }
+		public bool insertDongNhac(DongNhac dn)
+		{
+			string sql = "insert into Dong_nhac (Ten_dongnhac) values (N'" + dn.TenDongNhac + "')";
+			try
+			{
+				SqlConnection con = db.getConnection();
+				SqlCommand cmd = new SqlCommand(sql, con);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				return true;
+				cmd.Dispose();
+				con.Close();
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine("error insertDongNhac: " + ex.Message);
+				return false;
+			}
+		}
 
-        public void updateDongNhac(DongNhac ns)
-        {
-            string sql = "update Dong_nhac set Ten_dongnhac = N'" + ns.TenDongNhac + "' where Id = " + ns.Id;
+		public bool updateDongNhac(DongNhac ns)
+		{
+			string sql = "update Dong_nhac set Ten_dongnhac = N'" + ns.TenDongNhac + "' where Id = " + ns.Id;
+			try
+			{
+				SqlConnection con = db.getConnection();
+				SqlCommand cmd = new SqlCommand(sql, con);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				return true;
+				cmd.Dispose();
+				con.Close();
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine("error updateDongNhac: " + ex.Message);
+				return false;
+			}
+		}
 
-            SqlConnection con = db.getConnection();
-            SqlCommand cmd = new SqlCommand(sql, con);
-            con.Open();
-            cmd.ExecuteNonQuery();
-            cmd.Dispose();
-            con.Close();
-        }
+		public DongNhac getDongNhacByID(int id)
+		{
+			string sql = "select * from Dong_nhac where Id = " + id;
+			DataTable dt = new DataTable();
+			SqlConnection con = db.getConnection();
+			SqlDataAdapter da = new SqlDataAdapter(sql, con);
+			con.Open();
+			da.Fill(dt);
+			con.Close();
+			DongNhac dn;
+			int i = 0;
+			int ID = Convert.ToInt32(dt.Rows[i]["Id"].ToString());
+			String TenDongNhac = dt.Rows[i]["Ten_dongnhac"].ToString();
+			dn = new DongNhac(ID, TenDongNhac);
+			return dn;
 
-        public DongNhac getDongNhacByID(int id)
-        {
-            string sql = "select * from Dong_nhac where Id = " + id;
-            DataTable dt = new DataTable();
-            SqlConnection con = db.getConnection();
-            SqlDataAdapter da = new SqlDataAdapter(sql, con);
-            con.Open();
-            da.Fill(dt);
-            con.Close();
-            DongNhac dn;
-            int i = 0;
-            int ID = Convert.ToInt32(dt.Rows[i]["Id"].ToString());
-            String TenDongNhac = dt.Rows[i]["Ten_dongnhac"].ToString();
-            dn = new DongNhac(ID, TenDongNhac);
-            return dn;
+		}
 
-        }
-
-        public void deleteDongNhac(int id)
-        {
-            string sql = "delete from Dong_nhac where Id =" + id;
-            SqlConnection con = db.getConnection();
-            SqlCommand cmd = new SqlCommand(sql, con);
-            con.Open();
-            cmd.ExecuteNonQuery();
-            cmd.Dispose();
-            con.Close();
-        }
-    }
+		public bool deleteDongNhac(int id)
+		{
+			string sql = "delete from Dong_nhac where Id =" + id;
+			try
+			{
+				SqlConnection con = db.getConnection();
+				SqlCommand cmd = new SqlCommand(sql, con);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				return true;
+				cmd.Dispose();
+				con.Close();
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine("error deleteDongNhac: " + ex.Message);
+				return false;
+			}
+		}
+	}
 }
